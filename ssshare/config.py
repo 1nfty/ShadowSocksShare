f57df2d8753565188c5aa59a786acdc2743c5c07
Mr.Robot <31414964+1nfty@users.noreply.github.com>
2019-12-02 22:04:54 +0800

    删除无效订阅 增加订阅源与网站

--- ssshare/config.py
+++ ssshare/config.py
@@ -6,25 +6,26 @@ url = [
     'https://fanqiang.network/free-shadowsocksr-accounts-page-3',
     'https://fanqiang.network/free-shadowsocksr-accounts-page-4',
     'https://fanqiang.network/free-shadowsocksr-accounts-page-5',
-    'https://github.com/Steve-ShadowsocksR/-SSR-/blob/master/%E5%B0%8F%E9%A3%9E%E6%9C%BA%E7%BA%AF%E5%85%AC%E7%9B%8ASSR%E5%88%86%E4%BA%AB%EF%BC%81',
     'https://lncn.org/',
     'https://nulastudio.org/Freedom/',
     'https://t.me/s/SSRSUB',
     'https://t.me/s/SSRlist',
-    'https://t.me/s/gyjclub',
-    'https://www.goroutine.me/ssr/index.html',
+    'https://t.me/s/ssrshares',
+    'https://www.attackmen.com/',
+    'https://ssrtool.us/tool/share_ssr',
 ]
 
 subscriptions = [
+    'https://www.liesauer.net/yogurt/subscribe',
+    'https://raw.githubusercontent.com/eycorsican/rule-sets/master/kitsunebi_sub',
     'https://haobang.yangwang.workers.dev',
     'https://raw.githubusercontent.com/voken100g/AutoSSR/master/stable',
     'https://raw.githubusercontent.com/voken100g/AutoSSR/master/online',
     'https://raw.githubusercontent.com/voken100g/AutoSSR/master/recent',
+    'http://publicsub.getenjoyment.net/alluser.html',
     'https://muma16fx.netlify.com/',
     'https://qiaomenzhuanfx.netlify.com/',
-    'https://raw.githubusercontent.com/eycorsican/rule-sets/master/kitsunebi_sub',
     'https://raw.githubusercontent.com/ssrsub/ssr/master/ssrsub',
-    'https://teleplusfreessr.000webhostapp.com/',
-    'https://www.liesauer.net/yogurt/subscribe?ACCESS_TOKEN=DAYxR3mMaZAsaqUb',
-    'https://raw.githubusercontent.com/fggfffgbg/https-aishangyou.tube-/master/README.md'
+    'https://raw.githubusercontent.com/fggfffgbg/https-aishangyou.tube-/master/README.md',
+    'http://ss.pythonic.life/subscribe',
 ]
