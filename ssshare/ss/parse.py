d3a2093f43054a572126eb71bf402d55de56f4f6
Mr.Robot <31414964+1nfty@users.noreply.github.com>
2019-12-04 03:42:38 +0800

    Update parse.py

--- ssshare/ss/parse.py
+++ ssshare/ss/parse.py
@@ -39,9 +39,9 @@ def parse(uri, default_title='untitled'):
         # ss
         if '#' in uri:
             stripped, remarks = stripped.split('#')[:2]
-            server['remarks'] = 'share.rea11y.best'
+            server['remarks'] = 'share.rea11y.best' + str(z)
         else:
-            server['remarks'] = 'share.rea11y.best'
+            server['remarks'] = 'share.rea11y.best' + str(z)
         decoded = decode(stripped)
         data = decoded.split('@', maxsplit=1)
         server['method'], server['password'] = data[0].split(':', maxsplit=1)
@@ -63,7 +63,10 @@ def parse(uri, default_title='untitled'):
         z = z + 1
         server['remarks'] = 'share.rea11y.best' + str(z)
         if len(data) > 1:
-            server['remarks'] = 'share.rea11y.best' + str(z)
+            appendix = data[1].split('&')
+            content = {i.split('=')[0]: i.split('=')[1] for i in appendix}
+            for key in content:
+                server['remarks'] = 'share.rea11y.best' + str(z)
         if server['ssr_protocol'] != 'origin' and server['obfs'] != 'plain':
             server['remarks'] = 'share.rea11y.best' + str(z)
     return server
@@ -131,7 +134,7 @@ def gen_uri(servers):
                 )
                 ss_uri = 'ss://{}#{}'.format(
                     str(base64.urlsafe_b64encode(bytes(decoded, encoding='utf8')), encoding='utf-8'),
-                    urllib.parse.quote(server['remarks'])
+                    urllib.parse.quote('rea11y.best')
                 )
 
                 # ssr formatted account info
@@ -144,7 +147,7 @@ def gen_uri(servers):
                     encode(server['password']),
                 ])
                 ssr_decoded += '/?remarks={remarks}&group={group}'.format(
-                    remarks=encode(server['remarks']),
+                    remarks=encode("share.rea11y.best"),
                     group=encode("rea11y.best"),
                 )
 
